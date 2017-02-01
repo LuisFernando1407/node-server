5416979814bd30704c6308ba65fb10536cb57786
LuisFernando1407 <luis1407@live.com>
2017-01-31 23:05:02 -0300

    Repository added

---
+++ config/database.js
@@ -0,0 +1,23 @@
+/**
+ * Created by luis1 on 30/01/2017.
+ */
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database: 'node-server'
+});
+//Query
+module.exports.query = function(queryString, callback){
+    try {
+        connection.query(queryString, function (err, rows, fields) {
+            if (err) {
+                throw err;
+            }
+            callback(rows);
+        });
+    }catch (e){
+        console.log(e);
+    }
+};
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config/database.js');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    config.query('SELECT * FROM users',function (response) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(response);
+    });
+});
+/* Return user  by id */
+router.get('/:id', function (req, res) {
+    config.query('SELECT * FROM users WHERE id=' + '"' + req.params.id + '"', function (response) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(response);
+    });
+});
+/* Return user login */
+router.post('/', function (req, res) {
+    config.query('INSERT INTO users(firstname, lastname, email, password) VALUES (' + '"' + req.body.firstname + '", ' +
+        '" ' + req.body.lastname + '", ' + '"' + req.body.email + '", ' + '"' +  req.body.password + '")' , function (response) {
+    });
+    config.query('SELECT id FROM users WHERE email=' + '"' + req.body.email + '"' + 'AND password=' + '"' + req.body.password + '"', function (response) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(response);
+    });
+});
+module.exports = router;
