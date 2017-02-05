66d249bcb1c5da8fd97f233df8e827e06e09ba91
LuisFernando1407 <luis1407@live.com>
2017-02-05 01:43:46 -0300

    Correção de bugs e implementação de funções

--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,33 @@ router.get('/:id', function (req, res) {
         res.json(response);
     });
 });
-/* Return user  by email, password */
+/* Return user  by email, password or forgot password*/
 router.get('/:email/:password', function (req, res) {
-    config.query('SELECT * FROM users WHERE email=' + '"' + req.params.email + '"' + 'AND password=' + '"' + req.params.password + '"', function (response) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.json(response);
-    });
+    if(req.params.password === 'null'){
+        config.query('SELECT * FROM users WHERE email=' + '"' + req.params.email + '"', function (response) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.json(response);
+        });
+    }else{
+        config.query('SELECT * FROM users WHERE email=' + '"' + req.params.email + '"' + 'AND password=' + '"' + req.params.password + '"', function (response) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.json(response);
+        });
+    }
 });
 /* Return user login */
-router.post('/', function (req, res) {
+router.post('/user', function (req, res) {
     config.query('INSERT INTO users(firstname, lastname, email, password) VALUES (' + '"' + req.body.firstname + '", ' +
         '" ' + req.body.lastname + '", ' + '"' + req.body.email + '", ' + '"' +  req.body.password + '")' , function (response) {
     });
     config.query('SELECT id FROM users WHERE email=' + '"' + req.body.email + '"' + 'AND password=' + '"' + req.body.password + '"', function (response) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(response);
+    });
+});
+/* Returns everything if OK */
+router.put('/user', function (req, res) {
+    config.query('UPDATE users SET password=' + '"' +  req.body.newPassword + '"' + 'WHERE id=' + '"' + req.body.id + '"', function (response) {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(response);
     });
